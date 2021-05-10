f0aebde552fc5c2f840aae62e635f058320258fe
MelLilye <mattunkaito@gmail.com>
2021-05-10 12:28:31 +0900

    Character Update

--- Source/BakemonoBakari/BakemonoBakariCharacter.cpp
+++ Source/BakemonoBakari/BakemonoBakariCharacter.cpp
@@ -1,11 +1,24 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+//更新履歴
+// 2021/04/23 更新者：陳　攻撃について処理
+// 2021/05/04 更新者：陳　移動について処理
 
 #include "BakemonoBakariCharacter.h"
+#include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 ABakemonoBakariCharacter::ABakemonoBakariCharacter()
+	: IsAttack(false)       //攻撃フラグ 4/23
+	, IsJump(false)         //ジャンプフラグ 4/23
+	, IsCrouch(false)       //しゃがみフラグ 4/23
+	, IsDamage(false)       //ダメージフラグ 5/9
 {
+	//毎フレーム、クラスのTick()を呼ぶかどうかを決めるフラグ
+	PrimaryActorTick.bCanEverTick = true;
+
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
@@ -13,6 +26,20 @@ ABakemonoBakariCharacter::ABakemonoBakariCharacter()
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
+
+	//// Create a camera boom attached to the root (capsule)
+	//CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	//CameraBoom->SetupAttachment(RootComponent);
+	//CameraBoom->SetUsingAbsoluteRotation(true); // Rotation of the character should not affect rotation of boom
+	//CameraBoom->bDoCollisionTest = false;
+	//CameraBoom->TargetArmLength = 800.f;
+	//CameraBoom->SocketOffset = FVector(0.f,0.f,75.f);
+	//CameraBoom->SetRelativeRotation(FRotator(0.f,180.f,0.f));
+
+	//// Create a camera and attach to boom
+	//SideViewCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("SideViewCamera"));
+	//SideViewCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	//SideViewCameraComponent->bUsePawnControlRotation = false; // We don't want the controller rotating the camera
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Face in the direction we are moving..
@@ -28,6 +55,17 @@ ABakemonoBakariCharacter::ABakemonoBakariCharacter()
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
 
+void ABakemonoBakariCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ABakemonoBakariCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
 //////////////////////////////////////////////////////////////////////////
 // Input
 
@@ -37,10 +75,68 @@ void ABakemonoBakariCharacter::SetupPlayerInputComponent(class UInputComponent* 
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ABakemonoBakariCharacter::MoveRight);
+
+	PlayerInputComponent->BindAction("Attack", IE_Pressed, this, &ABakemonoBakariCharacter::Attack);
+
+	//PlayerInputComponent->BindTouch(IE_Pressed, this, &ABakemonoBakariCharacter::TouchStarted);
+	//PlayerInputComponent->BindTouch(IE_Released, this, &ABakemonoBakariCharacter::TouchStopped);
 }
 
 void ABakemonoBakariCharacter::MoveRight(float Value)
 {
-	// add movement in that direction
-	AddMovementInput(FVector(0.f,-1.f,0.f), Value);
+	if (IsAttack == false) //攻撃している時に移動できない　4/23
+	{
+		//FRotator StartRotation;
+		//FRotator EndRotation;
+		//StartRotation = GetActorRotation();
+		//EndRotation = GetActorRotation();
+		//EndRotation.Yaw = StartRotation.Yaw;
+
+		if (Value > 0.0)     //プレイヤーが必ず移動方向に回転する処理 5/4
+		{
+			//NewRotation() = FMath::Lerp(FRotator(0.f, 180.f, 0.f), FRotator(0.f, 0.f, 0.f), );
+			//SetActorRotation(FMath::RInterpTo(StartRotation, EndRotation, GetWorld()->GetDeltaSeconds(), 500));
+			SetActorRotation(FRotator(0.f, -90.f, 0.f));
+			// add movement in that direction
+			AddMovementInput(FVector(0.f, -1.f, 0.f), Value);
+		}
+		else
+		{
+			if (Value < 0.0)
+			{
+				//SetActorRotation(FMath::RInterpTo(StartRotation, EndRotation, GetWorld()->GetDeltaSeconds(), 500));
+				SetActorRotation(FRotator(0.f, 90.f, 0.f));
+				// add movement in that direction
+				AddMovementInput(FVector(0.f, -1.f, 0.f), Value);
+			}
+		}
+	}
+}
+
+//void ABakemonoBakariCharacter::TouchStarted(const ETouchIndex::Type FingerIndex, const FVector Location)
+//{
+//	// jump on any touch
+//	Jump();
+//}
+//
+//void ABakemonoBakariCharacter::TouchStopped(const ETouchIndex::Type FingerIndex, const FVector Location)
+//{
+//	StopJumping();
+//}
+
+//攻撃入力関数 4/23
+void ABakemonoBakariCharacter::Attack()
+{
+	IsAttack = true;
+}
+
+//ダメージを受ける処理 5/7
+void ABakemonoBakariCharacter::TakeDamage(int _dmg)
+{
+	m_info.hp -= _dmg;
+
+	if (m_info.hp < 0)
+	{
+		m_info.hp = 0;
+	}
 }
--- Source/BakemonoBakari/BakemonoBakariCharacter.h
+++ Source/BakemonoBakari/BakemonoBakariCharacter.h
@@ -1,4 +1,7 @@
-
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+//更新履歴
+//2021/04/23 更新者：陳　攻撃、しゃがみについて処理
+//2021/05/07 更新者：陳　プレイヤーの基本情報（HPなど）
 #pragma once
 
 #include "CoreMinimal.h"
@@ -8,18 +11,79 @@
 UCLASS(config=Game)
 class ABakemonoBakariCharacter : public ACharacter
 {
+	//プレイヤー情報格納用構造体 5/7
+	struct Player_Info
+	{
+		int hp;
+
+		Player_Info()
+			:hp(100)
+		{}
+	};
+
 	GENERATED_BODY()
+
+	///** Side view camera */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//class UCameraComponent* SideViewCameraComponent;
+
+	///** Camera boom positioning the camera beside the character */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//class USpringArmComponent* CameraBoom;
 
 protected:
 
+	//スタート時、または生成時に呼ばれる処理
+	virtual void BeginPlay() override;
+
 	/** Called for side to side input */
 	void MoveRight(float Val);
+
+	///** Handle touch inputs. */
+	//void TouchStarted(const ETouchIndex::Type FingerIndex, const FVector Location);
+
+	///** Handle touch stop event. */
+	//void TouchStopped(const ETouchIndex::Type FingerIndex, const FVector Location);
 
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 	// End of APawn interface
 
+	//ダメージを受ける処理 5/7
+	UFUNCTION(BlueprintCallable, Category = "MyFunctions")
+	void TakeDamage(int _dmg);
+
+	//攻撃しているかフラグ 4/23
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+		bool IsAttack;
+	//ジャンプしているかフラグ 4/23
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump")
+		bool IsJump;
+	//しゃがんでいるかフラグ 4/23
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Crouch")
+		bool IsCrouch;
+	//ダメージを受けているかフラグ 5/9
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		bool IsDamage;
 
 public:
 	ABakemonoBakariCharacter();
+
+	//Tick関数
+	virtual void Tick(float DeltaTime) override;
+
+	//攻撃入力関数 4/23
+	void Attack();
+
+	//HPを取得　5/7
+	UFUNCTION(BlueprintCallable, Category = "MyFunctions")
+		int GetHP() { return m_info.hp; }
+
+	///** Returns SideViewCameraComponent subobject **/
+	//FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
+	///** Returns CameraBoom subobject **/
+	//FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+private:
+	Player_Info m_info; //プレイヤー情報
 };
