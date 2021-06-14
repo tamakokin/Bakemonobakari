2ef5e0e2fbc3778fabda54f1670eb531a74127cb
Ogane000 <76036263+Ogane000@users.noreply.github.com>
2021-06-14 14:15:38 +0900

    Add files via upload

--- Source/BakemonoBakari/EnemyActor.cpp
+++ Source/BakemonoBakari/EnemyActor.cpp
@@ -76,14 +76,13 @@ void AEnemyActor::Tick(float DeltaTime)
 	{
 		return;
 	}
-
 	//			：2021/5/29 画面外にいる場合は動かないようにする（大金）
-	if (!m_pCheckInScreen->Check(GetActorLocation()))
-	{
+	if (!m_pCheckInScreen->Check(GetActorLocation())) 
+	{ 
 		Des();
-		return;
+		return; 
 	}
-	else if (m_EnemyState != ENEMY_STATE_DESTROY)
+	else 
 	{
 		Indication();
 	}
@@ -158,22 +157,247 @@ void AEnemyActor::EnemyStatusControl(float _deltaTime)
 	}
 }
 
+
+// 移動処理
+void AEnemyActor::EnemyMove(float _deltaTime)
+{
+	// ダメージ状態だったら移動しない
+	if (m_EnemyState == ENEMY_STATE_DAMAGE) return;
+
+	FVector tempEnemyPosition = GetActorLocation();
+	FRotator tempEnemyRotation = GetActorRotation();
+
+	ChangeAnim();
+
+	//switch (m_EnemyType)
+	//{
+
+	//case ENEMY_TYPE::ENEMY_TYPE_NONE:		// ダミー
+	//	break;
+
+	//case ENEMY_TYPE::ENEMY_TYPE_STOP:		// 動かない敵
+	//	// 動かない
+	//	break;
+
+	////case ENEMY_TYPE::ENEMY_TYPE_STRAIGHT:	// 真っすぐ突っ込んでくる敵
+
+	////	// 方向転換
+	////	if (m_SwitchVector)
+	////	{
+	////		if (!m_StraightVector)
+	////		{
+	////			// 前向き
+	////			tempEnemyRotation.Yaw = 90.f;
+	////		}
+	////		else
+	////		{
+	////			// 後ろ向き
+	////			tempEnemyRotation.Yaw = 270.f;
+	////		}
+
+	////		m_SwitchVector = false;
+	////		SetActorRotation(tempEnemyRotation);
+	////	}
+
+	////	m_prevEnemyPosition = tempEnemyPosition;
+
+	////	// 進む方向に応じて
+	////	if (!m_StraightVector)
+	////	{
+	////		// 前進
+	////		tempEnemyPosition.Y += m_moveSpeedY;
+	////	}
+	////	else
+	////	{
+	////		// 後退
+	////		tempEnemyPosition.Y -= m_moveSpeedY;
+	////	}
+
+	////	m_EnemyMovingDistance += m_moveSpeedY;
+
+	////	// 修正後のアクター位置を設定
+	////	SetActorLocation(tempEnemyPosition);
+
+	////	// 可動範囲を超えたら
+	////	if (tempEnemyPosition.Y > (m_initEnemyPosition.Y + m_moveRangeY) || tempEnemyPosition.Y < m_initEnemyPosition.Y)
+	////	{
+	////		m_SwitchVector = true;
+	////		m_StraightVector = !m_StraightVector;
+	////	}
+
+	////	m_EnemyState = ENEMY_STATE_MOVE;
+	////	ChangeAnim();
+
+	////	//---------------------------------------------------------------
+	////	// 段差があったら乗り越えるみたいなプログラムがあってもいいかも
+	////	//---------------------------------------------------------------
+	////	break;
+
+	////case ENEMY_TYPE::ENEMY_TYPE_JUMP:		// ジャンプしてくる敵
+
+	////	// 時間をカウント
+	////	m_EnemyJumpDeltaTime += _deltaTime;
+
+	////	// 待機時間を超えるまで動かない
+	////	if (m_EnemyJumpDeltaTime < m_JumpWait)	return;
+
+	////	//UE_LOG(LogTemp, Warning, TEXT("jumping"));
+
+	////	// 鉛直投げ上げ
+	////	tempDeltaZ = (m_InitVelocityZ - 0.5 * m_JumpGravity * ((m_EnemyJumpDeltaTime - m_JumpWait) * (m_EnemyJumpDeltaTime - m_JumpWait)));
+	////	tempEnemyPosition.Z += tempDeltaZ;
+
+	////	// 初期位置より下に行った場合補正してリターン
+	////	if (tempEnemyPosition.Z < m_initEnemyPosition.Z)
+	////	{
+	////		// 初期位置の戻ったら時間の初期化
+	////		SetActorLocation(tempEnemyPosition);
+	////		m_EnemyJumpDeltaTime = 0.f;
+	////		return;
+	////	}
+
+	////	// 値の反映
+	////	SetActorLocation(tempEnemyPosition);
+
+	////	m_EnemyState = ENEMY_STATE_MOVE;
+	////	ChangeAnim();
+
+	////	break;
+
+	//case ENEMY_TYPE::ENEMY_TYPE_JUMP_STRAIGHT:
+	//	//// 方向転換
+	//	////if (m_SwitchVector)
+	//	//{
+	//	//	if (!m_StraightVector)
+	//	//	{
+	//	//		// 前向き
+	//	//		tempEnemyRotation.Yaw = 90.f;
+	//	//	}
+	//	//	else
+	//	//	{
+	//	//		// 後ろ向き
+	//	//		tempEnemyRotation.Yaw = 270.f;
+	//	//	}
+
+	//	//	//m_SwitchVector = false;
+	//	//	SetActorRotation(tempEnemyRotation);
+	//	//}
+
+	//	//// 進む方向に応じて
+	//	//if (!m_StraightVector)
+	//	//{
+	//	//	// 前進
+	//	//	tempEnemyPosition.Y += m_moveSpeedY;
+	//	//}
+	//	//else
+	//	//{
+	//	//	// 後退
+	//	//	tempEnemyPosition.Y -= m_moveSpeedY;
+	//	//}
+
+	//	//// 修正後のアクター位置を設定
+	//	//SetActorLocation(tempEnemyPosition);
+
+	//	//// 可動範囲を超えたら
+	//	//if (tempEnemyPosition.Y > (m_initEnemyPosition.Y + m_moveRangeY) || tempEnemyPosition.Y < m_initEnemyPosition.Y)
+	//	//{
+	//	//	//m_SwitchVector = true;
+	//	//	m_StraightVector = !m_StraightVector;
+	//	//}
+
+	//	//// 時間をカウント
+	//	//m_EnemyJumpDeltaTime += _deltaTime;
+
+	//	//// 鉛直投げ上げ
+	//	//tempDeltaZ = (m_InitVelocityZ - 0.5 * m_JumpGravity * ((m_EnemyJumpDeltaTime - m_JumpWait) * (m_EnemyJumpDeltaTime - m_JumpWait)));
+	//	//tempEnemyPosition.Z += tempDeltaZ;
+
+	//	//// 初期位置より下に行った場合補正してリターン
+	//	//if (tempEnemyPosition.Z < m_initEnemyPosition.Z)
+	//	//{
+	//	//	// 初期位置の戻ったら時間の初期化
+	//	//	SetActorLocation(tempEnemyPosition);
+	//	//	m_EnemyJumpDeltaTime = 0.f;
+
+	//	//	m_EnemyState = ENEMY_STATE_MOVE;
+	//	//	ChangeAnim();
+	//	//	return;
+	//	//}
+
+	//	//// 値の反映
+	//	//SetActorLocation(tempEnemyPosition);
+	//	//m_EnemyState = ENEMY_STATE_MOVE;
+	//	ChangeAnim();
+
+	//	//break;
+
+
+	//case ENEMY_TYPE::ENEMY_TYPE_FIRING:		// 弾発射敵
+
+	//	if (tempPlayerCharacterPosition.Y >= tempEnemyPosition.Y)
+	//	{
+	//		// 前向き
+	//		tempEnemyRotation.Yaw = 90.f;
+	//		m_StraightVector = false;
+	//	}
+	//	else if (tempPlayerCharacterPosition.Y < tempEnemyPosition.Y)
+	//	{
+	//		// 後ろ向き
+	//		tempEnemyRotation.Yaw = 270.f;
+	//		m_StraightVector = true;
+	//	}
+	//	SetActorRotation(tempEnemyRotation);
+
+	//	break;
+
+	//case ENEMY_TYPE::ENEMY_TYPE_FRY:		// 空を飛ぶ敵
+	//	//---------------------------------------------------------------
+	//	// 今後によって書き換えます
+	//	//---------------------------------------------------------------
+	//	break;
+
+	//default:
+	//	break;
+}
+
+//// プレイヤーに当たった時の処理
+//void AEnemyActor::EnemyStop()
+//{
+//	switch (m_EnemyType)
+//	{
+//	case ENEMY_TYPE::ENEMY_TYPE_STRAIGHT:	// 真っすぐ突っ込んでくる敵
+//		// 方向転換
+//		if (m_EnemyMovingDistance > m_ChangeVectorTolerance)
+//		{
+//			//m_SwitchVector = true;
+//			m_StraightVector = !m_StraightVector;
+//		}
+//		break;
+//
+//	default:
+//		break;
+//	}
+//}
+
 // ダメージ関数
 void AEnemyActor::EnemyDamage()
 {
 	// ダメージ状態だったら
-	if ((m_EnemyState == ENEMY_STATE_DAMAGE) || (m_EnemyState == ENEMY_STATE_DESTROY)) return;
+	if ((m_EnemyState == ENEMY_STATE_DAMAGE)||(m_EnemyState == ENEMY_STATE_DESTROY)) return;
 
 	m_EnemyState = ENEMY_STATE_DAMAGE;
 	EnemyDamageEvent();
 
-	//------------------------------------------------
-	//hitエフェクト生成が入る
-	//------------------------------------------------
-	// HPを減らす。ここはプレイヤーの攻撃値を参照するようにしてもいいかも
-	--m_EnemyHP;
-	m_EnemyState = ENEMY_STATE_DAMAGE;
-	ChangeAnim();
+	if (m_EnemyHP > 0)
+	{
+		//------------------------------------------------
+		//hitエフェクト生成が入る
+		//------------------------------------------------
+		// HPを減らす。ここはプレイヤーの攻撃値を参照するようにしてもいいかも
+		--m_EnemyHP;
+		m_EnemyState = ENEMY_STATE_DAMAGE;
+		ChangeAnim();
+	}
 
 	if (m_EnemyHP <= 0)
 	{
